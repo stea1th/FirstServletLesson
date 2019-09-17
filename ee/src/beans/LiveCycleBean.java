7177bb11d25a03d9ad79669ca08449be7d424184
Vadim Vlasov <stea1th@mail.ru>
2019-09-17 14:13:59 +0200

    Interceptors for methods @ExcludeClassInterceptors

--- ee/src/beans/LiveCycleBean.java
+++ ee/src/beans/LiveCycleBean.java
@@ -1,20 +1,25 @@
 package beans;
 
+import interceptor.Interceptor;
+
 import javax.enterprise.context.RequestScoped;
+import javax.interceptor.ExcludeClassInterceptors;
 import javax.interceptor.Interceptors;
 
 @RequestScoped
-@Interceptors(interceptor.Interceptor.class)
+//@Interceptors(interceptor.Interceptor.class)
 public class LiveCycleBean {
 
     public LiveCycleBean() {
         System.out.println("Construct");
     }
 
+    @Interceptors({Interceptor.class})
     public void doJob(){
         System.out.println("Do job");
     }
 
+    @ExcludeClassInterceptors
     public void doJob2() {
         System.out.println("Do job 2");
     }
